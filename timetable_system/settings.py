4ddd7ca78606c9c80119d86dccd749e29a870d27
ronnkarani <karanironny25@gmail.com>
2025-11-03 20:07:27 +0300

    allow  host

--- timetable_system/settings.py
+++ timetable_system/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-1mmxvo)&wx9dzcg7m71&3i9um-zq&6cwmu42-k*j8dikloe=@)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =False
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
